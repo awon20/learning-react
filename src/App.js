412c9e9dfa2dddcc1043d52784d6ea90f8fdba6e
Armel Wonga <Armel.Wonga@w-hs.de>
2021-09-17 12:13:16 +0200

    add a new component FormatName

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
-const name = "Josh Perez";
-const element = <h1>Hello, {name}</h1>;
+// const name = "Gaello Armelito";
+// const element = <h1>Hello, {name}</h1>;
+import { FormatName } from "./components/FormatName";
+
+const user = {
+  firstName: "Benji",
+  lastName: "Spencer",
+};
+const element = <h1>Hello, {FormatName(user)}!</h1>;
 
 function App() {
   return <div className="App">{element}</div>;
